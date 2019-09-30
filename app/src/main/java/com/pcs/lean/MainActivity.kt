8ff3a32777098d14f62fc92eabec3c7baa549ca6
Victor Chisvert <victor.chisvert@pcs.es>
2019-09-30 16:59:45 +0200

    Navigation Drawer and Fragment done

--- app/src/main/java/com/pcs/lean/MainActivity.kt
+++ app/src/main/java/com/pcs/lean/MainActivity.kt
@@ -2,43 +2,105 @@ package com.pcs.lean
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.MenuItem
+import androidx.annotation.IdRes
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+
+import com.google.android.material.navigation.NavigationView
+import com.pcs.lean.fragment.HistoryFragment
+import com.pcs.lean.fragment.SettingFragment
+import com.pcs.lean.fragment.WarningFragment
+
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private var textView : TextView? = null
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.test)
+       toolbar = findViewById(R.id.toolbar)
+       setSupportActionBar(toolbar)
 
-        getOFs()
+       drawerLayout = findViewById(R.id.drawer_layout)
+       val toggle = ActionBarDrawerToggle(
+           this, drawerLayout, toolbar, 0, 0
+       )
+       drawerLayout.addDrawerListener(toggle)
+       toggle.syncState()
+
+       navView = findViewById(R.id.nav_view)
+       navView.setNavigationItemSelectedListener(this)
+
+       navigateToHome()
     }
 
-    fun getOFs(){
-        val sdf = SimpleDateFormat("dd/M/yyyy")
-        val currentDate = sdf.format(Date())
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
-        val queue = Volley.newRequestQueue(this)
-        val url :String ="http://172.16.0.173/Apps/lean/android/router.php?action=get-ofs&date=${currentDate}"
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_warning -> {
+                navigateToHome()
+            }
+            R.id.nav_history -> {
+                navigateToFragment(
+                    fragment = HistoryFragment(),
+                    allowStateLoss = false,
+                    containerViewId = R.id.fragment_container
+                )
+                setTitle("Historico")
+            }
+            R.id.nav_settings -> {
+                navigateToFragment(
+                    fragment = SettingFragment(),
+                    allowStateLoss = false,
+                    containerViewId = R.id.fragment_container
+                )
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
-        val request = StringRequest(Request.Method.GET, url,
-            Response.Listener<String> { response ->
-                textView!!.text=response
-            },
-            Response.ErrorListener { err ->
-                textView!!.text = err.toString()
-            })
+    private fun navigateToHome(){
+        navigateToFragment(
+            fragment = WarningFragment(),
+            allowStateLoss = false,
+            containerViewId = R.id.fragment_container
+        )
+        setTitle("Incidencias")
+    }
 
-        queue.add(request)
+    private fun navigateToFragment(fragment: Fragment, allowStateLoss: Boolean = false, @IdRes containerViewId: Int){
+        val ft = supportFragmentManager
+                    .beginTransaction()
+                    .replace(containerViewId, fragment)
+        if (!supportFragmentManager.isStateSaved ){
+            ft.commit()
+        }
+        else if (allowStateLoss){
+            ft.commitAllowingStateLoss()
+        }
     }
 
 }
---
+++ app/src/main/java/com/pcs/lean/fragment/WarningFragment.kt
@@ -0,0 +1,21 @@
+package com.pcs.lean.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pcs.lean.R
+
+class WarningFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_warning, container, false)
+        return root;
+    }
+
+}
